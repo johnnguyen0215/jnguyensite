d418a44c5e601f71144b28265d631f8f1308ab2f
John <jenguyen@uci.edu>
2015-10-29 01:40:12 -0700

    edit

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,3 +1,39 @@
-$(document).ready(function(){
 
-})
+$(document).ready(function(){
+	$("#scrollDownButton").click(function(){
+		$("#aboutPage").ScrollTo({
+			duration: 1000
+		});
+	});
+	$("#projectButton").click(function(){
+		$("#projectsPage").ScrollTo({
+			duration: 1000
+		});
+	});
+	$("#navHomeBtn").click(function(){
+		$("#homePage").ScrollTo({
+			duration: 1000
+		});
+	});
+	$("#navAboutBtn").click(function(){
+		$("#aboutPage").ScrollTo({
+			duration: 1000
+		});
+	});
+	$("#navResumeBtn").click(function(){
+		$("#skillsPage").ScrollTo({
+			duration: 1000
+		});
+	});
+});
+
+$(window).scroll(function(){
+	var scroll = $(window).scrollTop();
+
+	if (scroll >= 500){
+		$(".navbar").addClass("navBg");
+	}
+	else if (scroll < 500){
+		$(".navbar").removeClass("navBg");
+	}
+});

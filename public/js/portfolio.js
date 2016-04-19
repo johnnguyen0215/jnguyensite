24f28ef5cea2163a021ea0a78162b48867aefe0b
John <jenguyen@uci.edu>
2016-04-19 11:35:53 -0700

    initialized

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -6,6 +6,12 @@ $(document).ready(function(){
 		});
 		return false;
 	});
+	$("#scrollUpButton").click(function(){
+		$("#homePage").ScrollTo({
+			duration: 1000
+		});
+		return false;
+	});
 	$("#projectsButton").click(function(){
 		$("#projectsPage").ScrollTo({
 			duration: 1000
@@ -34,12 +40,26 @@ $(document).ready(function(){
 		$("#contactPage").ScrollTo({
 			duration: 1000
 		});
+		return false;
+	});
+	$("#navProjectsBtn").click(function(){
+		$("#projectsPage").ScrollTo({
+			duration: 1000
+		});
+		return false;
+	});
+	$("#navFindingsBtn").click(function(){
+		$("#findingsPage").ScrollTo({
+			duration: 1000
+		});
+		return false;
 	});
 
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
 		$('#navList').toggleClass('show');
 	});
+
 
 	$(window).scroll(function(event){
 		var scroll = $(window).scrollTop();
@@ -49,7 +69,15 @@ $(document).ready(function(){
 		else if (scroll < 500){
 			$(".navbar").removeClass("navBg");
 		}
+		
+		if (scroll > 500){
+			$("#scrollUpButton").css("display","block");
+		}
+		else{
+			$("#scrollUpButton").css("display","none");
+		}
 	});
+
 
 
 });
